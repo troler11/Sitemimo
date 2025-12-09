1aa6606c93b74e0e3c18df5eed80d316815f543a
troler11 <lucasbubu22@gmail.com>
2025-12-08 23:10:48 -0300

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { login } from './controllers/authController';
 import { getDashboardData } from './controllers/dashboardController';
+import { calculateRoute } from './controllers/mapController'; // <--- Importe
 import { verifyToken } from './middleware/auth';
 
 const router = Router();
@@ -10,6 +11,7 @@ router.post('/login', login);
 
 // Rotas Protegidas
 router.get('/dashboard', verifyToken, getDashboardData);
+router.get('/rota/:tipo/:placa', verifyToken, calculateRoute);
 // router.get('/admin/users', verifyToken, getUsers); // Implementar depois
 // router.post('/admin/users', verifyToken, createUser); // Implementar depois
 
