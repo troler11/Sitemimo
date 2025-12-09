d0396ace49e3165f3b04efe0e9337e68f43b3156
troler11 <lucasbubu22@gmail.com>
2025-12-08 22:56:36 -0300

    Create webpack.config.js

---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Crie um HTML básico com <div id="root"></div>
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true, // Para React Router funcionar
+    port: 8080,
+  },
+};
