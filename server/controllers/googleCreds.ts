f914b144ef8d1307641faf9a45f7061ade15730d
troler11 <lucasbubu22@gmail.com>
2026-04-16 14:32:40 -0300

    Create googleCreds.ts

---
+++ server/controllers/googleCreds.ts
@@ -0,0 +1,15 @@
+// Arquivo: server/controllers/googleCreds.ts
+
+export const credenciais = {
+  "type": "service_account",
+  "project_id": "mimo-painel-escala",
+  "private_key_id": "abc123def456...",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgk...\n-----END PRIVATE KEY-----\n",
+  "client_email": "bot-planilha@...",
+  "client_id": "111...",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://...",
+  "universe_domain": "googleapis.com"
+};
